146699030ce97ba0c2dc677decc14c41e0e7c9ca
Seth Kokou SODJI <98849205+sethisbrothers@users.noreply.github.com>
2022-05-13 08:13:02 +0000

    end

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Création des variables permettant de récupérer certains éléments
 var ul = document.getElementsByTagName("ul")[0];
 var myList = document.getElementsByTagName("li");
 var myListSpan = document.getElementsByTagName("span");
 
+//Création d'une fonction permettant de pouvoir cocher les éléments de la liste
 function checked() {
     var classLi = this.getAttribute("class");
     if (classLi === "checked") {
@@ -11,10 +13,13 @@ function checked() {
         this.setAttribute("class", "checked");
     }
 }
+
+//Création d'une fonction permettant d'éffacer les élements de la liste une fois qu'on click sur la croix
 function deleteToDo() {
     ul.removeChild(this.parentElement);
 }
 
+//l'instruction donnée au bouton une fois que l'utilisateur aura à cliquer sur le boutton "Ajouter"
 function addToDo() {
     var todo = document.getElementById("todo").value;
     if (todo === "") {
@@ -33,6 +38,7 @@ function addToDo() {
     }
 }
 
+//Création de gestionnaire d'évenement 
 for (var i = 0; i < myList.length; i++) {
     myListSpan[i].addEventListener("click", deleteToDo);
     myList[i].addEventListener("click", checked);
